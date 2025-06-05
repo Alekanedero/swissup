309f8fb54cbb49dd768d25c3dc3a1dd7d508a358
Alekanedero <alexandrecompin@gmail.com>
2025-06-05 22:19:50 +0200

    vercel

--- app/offres/offre.tsx
+++ app/offres/offre.tsx
@@ -1,12 +1,10 @@
-export type OffreProps = {
-  
-};
+export type OffreProps = {};
 
 export const Offre = (props: OffreProps) => {
   return (
     <div>
-    <p>Offre</p>
-    <p>{props.}</p>
+      <p>Offre</p>
+      <p></p>
     </div>
   );
 };
