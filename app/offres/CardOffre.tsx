9b936ce035accd1511ed53198e2402054ce4f083
Alekanedero <alexandrecompin@gmail.com>
2025-06-05 22:34:30 +0200

    vercel 3

---
+++ app/offres/CardOffre.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader } from "@/components/ui/card";
+
+export type OffreProps = {};
+
+export const CardOffre = (props: OffreProps) => {
+  return (
+    <Card>
+      <CardHeader>Offre 1</CardHeader>
+    </Card>
+  );
+};
--- app/offres/page.tsx
+++ app/offres/page.tsx
@@ -1,7 +1,10 @@
+import { CardOffre } from "./CardOffre";
+
 export default function Offre() {
   return (
     <div className="">
       <h2>Page offres</h2>
+      <CardOffre />
     </div>
   );
 }
