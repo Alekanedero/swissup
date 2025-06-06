88c8a76eb1c0d2ce6cf064355f38ec740018d56f
Alekanedero <alexandrecompin@gmail.com>
2025-06-06 14:16:54 +0200

    #7 magicLink

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,14 @@
 import { createAuthClient } from "better-auth/client";
-import { inferAdditionalFields } from "better-auth/client/plugins";
+import {
+  inferAdditionalFields,
+  magicLinkClient,
+} from "better-auth/client/plugins";
 import { auth } from "./auth";
 
 export const authClient = createAuthClient({
   // TODO : Change this to the production URL
   baseURL: "http://localhost:3000",
-  plugins: [inferAdditionalFields<typeof auth>()],
+  plugins: [magicLinkClient(), inferAdditionalFields<typeof auth>()],
 });
+
+export const { useSession } = authClient;
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,47 @@
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { betterAuth } from "better-auth";
-import { PrismaClient } from "@/app/generated/prisma";
+import { prisma } from "./prisma";
 import { nextCookies } from "better-auth/next-js";
-
-const client = new PrismaClient();
+import { magicLink } from "better-auth/plugins/magic-link";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
-  database: prismaAdapter(client, {
+  database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   appName: "swissup",
-  plugins: [nextCookies()],
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      await resend.emails.send({
+        to: user.email,
+        from: "nextfullstack@nowts.app",
+        subject: "Reset your password",
+        text: `Click the link to reset your password: ${url}`,
+      });
+    },
   },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    },
+  },
+  plugins: [
+    magicLink({
+      sendMagicLink: async ({ email, url }) => {
+        await resend.emails.send({
+          to: email,
+          from: "swissup@gmail.com",
+          subject: "Magic Link",
+          text: `Click the link to sign in: ${url}`,
+        });
+      },
+    }),
+    nextCookies(),
+  ],
 });
