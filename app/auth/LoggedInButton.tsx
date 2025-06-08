3395277a1a8c0ae34246dd98e700c6106f54f067
Alekanedero <alexandrecompin@gmail.com>
2025-06-08 10:08:51 +0200

    signout

--- app/auth/LoggedInButton.tsx
+++ app/auth/LoggedInButton.tsx
@@ -2,35 +2,85 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { LogoutButton } from "./LogoutButton";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { User2 } from "lucide-react";
+import { Loader, LogOut, User2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useMutation } from "@tanstack/react-query";
+import { signOut } from "next-auth/react";
 
 export const LoggedInButton = ({
   user,
 }: {
   user: { name: string; email: string };
 }) => {
+  const mutation = useMutation({
+    mutationFn: async () => {
+      signOut();
+    },
+  });
   return (
     <>
       <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button size="sm">{user.name || user.email}</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem asChild>
-            <Link href="/auth">
-              <User2 size={12} className="mr-2" />
-              Account
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <LogoutButton />
-          </DropdownMenuItem>
-        </DropdownMenuContent>
+        <AlertDialog>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="sm">
+              {user.name || user.email}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem asChild>
+              <Link href="/auth">
+                <User2 size={12} className="mr-2" />
+                Account
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <AlertDialogTrigger>
+              <DropdownMenuItem asChild>
+                <LogoutButton />
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+          </DropdownMenuContent>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure you want to logout?
+              </AlertDialogTitle>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel asChild>
+                <Button variant="secondary">Cancel</Button>
+              </AlertDialogCancel>
+              <Button
+                variant="destructive"
+                onClick={() => {
+                  mutation.mutate();
+                }}
+                disabled={mutation.isPending}
+              >
+                {mutation.isPending ? (
+                  <Loader size={12} className="mr-2" />
+                ) : (
+                  <LogOut size={12} className="mr-2" />
+                )}
+                Logout
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </DropdownMenu>
     </>
   );
