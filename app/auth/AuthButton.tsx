b48d017aeab02e7795c872964d6a6dae3745077a
Alekanedero <alexandrecompin@gmail.com>
2025-06-08 09:23:00 +0200

    link

--- app/auth/AuthButton.tsx
+++ app/auth/AuthButton.tsx
@@ -1,13 +1,13 @@
 import { getUser } from "@/lib/auth-session";
 import { LoggedInButton } from "./LoggedInButton";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export const AuthButton = async () => {
   const session = await getUser();
-  const router = useRouter();
 
   if (!session) {
-    return router.push("/auth/signin");
+    return <Link href={"/auth/sigin"}>Signin</Link>;
+    // router.push("/auth/signin");
   }
 
   return <LoggedInButton user={session} />;
