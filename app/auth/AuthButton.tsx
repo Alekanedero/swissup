01921ff695505359766abee6594424c83dd7e0b5
Alekanedero <alexandrecompin@gmail.com>
2025-06-08 09:35:15 +0200

    vercel

--- app/auth/AuthButton.tsx
+++ app/auth/AuthButton.tsx
@@ -1,13 +1,20 @@
 import { getUser } from "@/lib/auth-session";
 import { LoggedInButton } from "./LoggedInButton";
 import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export const AuthButton = async () => {
   const session = await getUser();
 
   if (!session) {
-    return <Link href={"/auth/signin"}>Signin</Link>;
-    // router.push("/auth/signin");
+    return (
+      <Link
+        href={"/auth/signin"}
+        className={buttonVariants({ size: "sm", variant: "outline" })}
+      >
+        Signin
+      </Link>
+    );
   }
 
   return <LoggedInButton user={session} />;
