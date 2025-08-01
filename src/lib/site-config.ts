5655a48bed57eef24fca30140b1dc15505a370db
Alekanedero <116439305+Alekanedero@users.noreply.github.com>
2025-08-01 20:32:47 +0200

    Update site-config.ts

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,5 +1,5 @@
 export const SiteConfig = {
-  title: "Swissup",
+  title: "Swiss-Z",
   description:
     "L'app pour réussir votre expatriation en Suisse : emploi, démarches, mise en relation avec les bons contacts. Tout pour s’installer sereinement.",
   icons: {
