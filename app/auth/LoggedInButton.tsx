badf8667c8b40df010c5def7bca6edbcddfa9aa5
Alekanedero <alexandrecompin@gmail.com>
2025-08-04 23:37:45 +0200

    avatar loggedinbutton

--- app/auth/LoggedInButton.tsx
+++ app/auth/LoggedInButton.tsx
@@ -9,16 +9,23 @@ import { LogoutButton } from "./LogoutButton";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { User2 } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const LoggedInButton = ({
   user,
 }: {
-  user: { name: string; email: string };
+  user: { name: string; email: string; image?: string };
 }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant={"outline"} size="sm">
+          <Avatar className="h-5 w-5 ">
+            <AvatarFallback>{user.email?.[0]}</AvatarFallback>
+            {user.image && (
+              <AvatarImage src={user.image} alt={user.email ?? ""} />
+            )}
+          </Avatar>
           {user.name || user.email}
         </Button>
       </DropdownMenuTrigger>
